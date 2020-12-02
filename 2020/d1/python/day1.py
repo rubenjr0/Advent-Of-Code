db6a2c25044f59aebd4753b2996c41c4e074820f
Ruben <megarex333@gmail.com>
2020-12-02 18:31:29 +0100

    Refactor

---
+++ 2020/d1/python/day1.py
@@ -0,0 +1,20 @@
+def get_challenge1_answer(entries):
+    for i in range(0, len(entries)):
+        for j in range(i + 1, len(entries)):
+            if entries[i] + entries[j] == 2020:
+                return entries[i] * entries[j]
+
+def get_challenge2_answer(entries):
+    for i in range(0, len(entries)):
+        for j in range(i + 1, len(entries)):
+            for k in range(j + 1, len(entries)):
+                if entries[i] + entries[j] + entries[k] == 2020:
+                    return entries[i] * entries[j] * entries[k]
+
+f = open('d1\\input.txt')
+entries = list(map(lambda x: int(x.strip()), f.readlines()))
+answer1 = get_challenge1_answer(entries)
+answer2 = get_challenge2_answer(entries)
+
+print(answer1)
+print(answer2)
---
+++ 2020/d2/python/challenge1.py
@@ -0,0 +1,25 @@
+def challenge1_validation(entry):
+    policy, char, password = entry
+    min, max = list(map(lambda p: int(p), policy.split('-')))
+    return min <= password.count(char) <= max
+
+def challenge2_validation(entry):
+    policy, char, password = entry
+    i1, i2 = list(map(lambda p: int(p) - 1, policy.split('-')))
+    return (password[i1] == char and not password[i2] == char) or (not password[i1] == char and password[i2] == char)
+
+
+
+f = open('d2\\input.txt')
+passwords = list(
+    map(
+        lambda x: x.replace(':', '').split(),
+        f.readlines()
+    )
+)
+
+answer1 = len([x for x in passwords if challenge1_validation(x)])
+answer2 = len([x for x in passwords if challenge2_validation(x)])
+
+print(answer1)
+print(answer2)
