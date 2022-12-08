506d757a7596e5f11fd1ebe9c3cb6652ec66f363
Rubén <rubenjr0@proton.me>
2022-12-08 05:01:57 +0100

    ⚗️ Benchmarks

--- 2022/day_7/src/main.rs
+++ 2022/day_7/src/main.rs
@@ -2,12 +2,15 @@ mod computer;
 mod filesystem;
 mod node;
 
+use std::time::Instant;
+
 use computer::Computer;
 
 fn main() {
     let input = include_str!("../input.txt");
 
     let mut computer = Computer::new();
+    let computer_creation_time = Instant::now();
     input
         .lines()
         .filter(|l| l != &"$ ls")
@@ -15,27 +18,36 @@ fn main() {
         .for_each(|i| {
             computer.interpret_entry(&i);
         });
+    let computer_creation_time = computer_creation_time.elapsed();
+    println!("Computer created in {computer_creation_time:?}\n");
 
+    let solution_time = Instant::now();
     let small_directories = computer.small_directories();
+    let solution_time = solution_time.elapsed();
     let solution = small_directories.iter().map(|n| n.size()).sum::<usize>();
-    // assert_eq!(solution, 1118405);
-    println!("Part 1: {} small directories:", small_directories.len());
+    println!(
+        "Part 1: {} small directories found in {solution_time:?}:",
+        small_directories.len()
+    );
     for dir in &small_directories {
         println!(" - {}", dir.name());
     }
-    println!(" >> Total size: {} bytes", solution);
+    println!(" # Total size: {} bytes", solution);
 
     println!();
 
     print!("Part 2: ");
+    let deletion_candidate_time = Instant::now();
     let deletion_candidate = computer.deletion_candidate();
+    let deletion_candidate_time = deletion_candidate_time.elapsed();
     if let Some(candidate) = deletion_candidate {
-        println!(
+        print!(
             "Deletion candidate: {} ({} bytes)",
             candidate.name(),
             candidate.size()
         );
     } else {
-        println!("No deletion candidate found");
+        print!("No deletion candidate found");
     }
+    println!(" found in {deletion_candidate_time:?}");
 }
