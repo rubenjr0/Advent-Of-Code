8179f6a48ba914bbb867deba2ca47cf3b64ade59
Rubén <rubenjr0@proton.me>
2022-12-13 00:19:36 +0100

    ✨ Day 12

--- 2022/day_12/src/height_map.rs
+++ 2022/day_12/src/height_map.rs
@@ -1,19 +1,18 @@
-use std::fmt::Display;
-
 use eyre::Result;
+use pathfinding::{num_traits::Zero, prelude::bfs};
+use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+
+use crate::point::Point;
 
 type Grid = Vec<Vec<u8>>;
-type Coordinate = (usize, usize);
 
 #[derive(Debug)]
 pub struct HeightMap {
     grid: Grid,
     height: usize,
     width: usize,
-    objective: Coordinate,
-    path: Vec<Coordinate>,
-    best_path: Option<Vec<Coordinate>>,
-    visited: Vec<Coordinate>,
+    start: Point,
+    objective: Point,
 }
 
 impl HeightMap {
@@ -42,111 +41,81 @@ impl HeightMap {
         let height = grid.len();
         let width = grid[0].len();
         let starting_point = starting_point.unwrap();
+        let starting_point = Point::new(starting_point.1, starting_point.0);
+        let objective = objective.unwrap();
+        let objective = Point::new(objective.1, objective.0);
         Ok(HeightMap {
             grid,
             height,
             width,
-            objective: objective.unwrap(),
-            path: vec![starting_point],
-            best_path: None,
-            visited: vec![starting_point],
+            start: starting_point,
+            objective: objective,
         })
     }
 
-    pub fn distance_to_objective(&self, (y, x): &Coordinate) -> usize {
-        let (yf, xf) = self.objective;
-        y.abs_diff(yf) + x.abs_diff(xf)
+    fn get_coordinate(&self, point: &Point) -> u8 {
+        self.grid[point.y][point.x]
     }
 
-    fn get_coordinate(&self, (y, x): &Coordinate) -> u8 {
-        self.grid[*y][*x]
-    }
-
-    fn can_move(&self, from: &Coordinate, to: &Coordinate) -> bool {
+    fn can_move(&self, from: &Point, to: &Point) -> bool {
         let a = self.get_coordinate(from);
         let b = self.get_coordinate(to);
         b <= a + 1
     }
 
-    pub fn move_candidates(&self) -> Vec<(Coordinate, usize)> {
-        let position = self.path.last().unwrap();
+    pub fn move_candidates(&self, point: &Point) -> impl Iterator<Item = Point> {
         let mut candidates = vec![];
-        if position.0 > 0 {
-            let p = (position.0 - 1, position.1);
-            if self.can_move(position, &p) {
-                candidates.push(p);
+        if point.x > 0 {
+            let left = point.left();
+            if self.can_move(point, &left) {
+                candidates.push(left);
             }
         }
-        if position.0 < self.height - 1 {
-            let p = (position.0 + 1, position.1);
-            if self.can_move(position, &p) {
-                candidates.push(p);
+        if point.x < self.width - 1 {
+            let right = point.right();
+            if self.can_move(point, &right) {
+                candidates.push(right);
             }
         }
-        if position.1 > 0 {
-            let p = (position.0, position.1 - 1);
-            if self.can_move(position, &p) {
-                candidates.push(p);
+        if point.y > 0 {
+            let top = point.top();
+            if self.can_move(point, &top) {
+                candidates.push(top);
             }
         }
-        if position.1 < self.width - 1 {
-            let p = (position.0, position.1 + 1);
-            if self.can_move(position, &p) {
-                candidates.push(p);
+        if point.y < self.height - 1 {
+            let bottom = point.bottom();
+            if self.can_move(point, &bottom) {
+                candidates.push(bottom);
             }
         }
-        let mut candidates: Vec<_> = candidates
-            .into_iter()
-            .filter(|c| !(self.path.contains(c) || self.visited.contains(c)))
-            .map(|c| (c, self.distance_to_objective(&c)))
-            .collect();
-        candidates.sort_by_key(|c| c.1);
-        candidates
+        candidates.into_iter()
     }
 
-    pub fn solve(&mut self) -> &Option<Vec<Coordinate>> {
-        self.backtrack();
-        &self.best_path
+    pub fn find_shortest_possible(&self) -> Option<Vec<Point>> {
+        self.grid
+            .par_iter()
+            .enumerate()
+            .flat_map(|(y, r)| {
+                r.par_iter()
+                    .enumerate()
+                    .filter(|(_, p)| p.is_zero())
+                    .flat_map(move |(x, _)| self.find_shortest_path_bfs(&Point { x, y }))
+                    .min_by_key(|s| s.len())
+            })
+            .min_by_key(|s| s.len())
     }
 
-    fn backtrack(&mut self) {
-        println!("{self}");
-        if self.path.last().unwrap() == &self.objective {
-            if self.best_path.is_none() || self.path.len() < self.best_path.as_ref().unwrap().len()
-            {
-                let mut best_path = self.path.clone();
-                best_path.pop();
-                self.best_path = Some(best_path);
-            }
-            return;
-        }
-        if self.best_path.is_some() && self.path.len() >= self.best_path.as_ref().unwrap().len() {
-            return;
-        }
-        let candidates = self.move_candidates();
-        for (candidate, _) in candidates {
-            self.path.push(candidate);
-            self.visited.push(candidate);
-            self.backtrack();
-            self.path.pop();
-        }
+    pub fn find_shortest_path(&self) -> Option<Vec<Point>> {
+        self.find_shortest_path_bfs(&self.start)
     }
-}
 
-impl Display for HeightMap {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        for (ri, row) in self.grid.iter().enumerate() {
-            for (ci, col) in row.iter().enumerate() {
-                if self.path.contains(&(ri, ci)) {
-                    write!(f, " # ")?;
-                    continue;
-                }
-                write!(f, " · ")?
-            }
-            writeln!(f, "")?
-        }
-        Ok(())
+    fn find_shortest_path_bfs(&self, start: &Point) -> Option<Vec<Point>> {
+        bfs(
+            start,
+            |p| self.move_candidates(p).collect::<Vec<_>>(),
+            |p| p == &self.objective,
+        )
     }
 }
 
--- 2022/day_12/src/main.rs
+++ 2022/day_12/src/main.rs
@@ -1,17 +1,37 @@
+use std::time::Instant;
+
 use eyre::Result;
 use height_map::HeightMap;
 
 mod height_map;
+mod point;
 
 fn main() -> Result<()> {
     let input = include_str!("../input.txt");
-    let mut height_map = HeightMap::parse(input)?;
-    // println!("{height_map}");
-    let solution = height_map.solve();
-    if let Some(solution) = solution {
-        println!("Solved in {} steps", solution.len());
+    let height_map = HeightMap::parse(input)?;
+    let bfs_timer = Instant::now();
+    let solution = height_map.find_shortest_path();
+    let bfs_timer = bfs_timer.elapsed();
+    println!("Part one");
+    if let Some(shortest_path) = solution {
+        println!(
+            " - The shortest path has {} steps [{bfs_timer:?}]",
+            shortest_path.len() - 1
+        );
     } else {
-        println!("No solution found");
+        println!(" - There's no shortest path D:");
+    }
+    let bfs_timer = Instant::now();
+    let solution = height_map.find_shortest_possible();
+    let bfs_timer = bfs_timer.elapsed();
+    println!("Part two");
+    if let Some(shortest_path) = solution {
+        println!(
+            " - The shortest possible path has {} steps [{bfs_timer:?}]",
+            shortest_path.len() - 1
+        );
+    } else {
+        println!(" - There's no shortest path D:");
     }
     Ok(())
 }
@@ -23,8 +43,18 @@ mod tests {
     #[test]
     fn part_one() {
         let test = include_str!("../test.txt");
-        let mut height_map = HeightMap::parse(test).unwrap();
-        let solution = height_map.solve();
-        assert_eq!(solution.as_ref().unwrap().len(), 31);
+        let height_map = HeightMap::parse(test).unwrap();
+        let solution = height_map.find_shortest_path();
+        assert!(solution.is_some());
+        assert_eq!(solution.expect("No solution").len() - 1, 31);
+    }
+
+    #[test]
+    fn part_two() {
+        let test = include_str!("../test.txt");
+        let height_map = HeightMap::parse(test).unwrap();
+        let solution = height_map.find_shortest_possible();
+        assert!(solution.is_some());
+        assert_eq!(solution.expect("No solution").len() - 1, 29);
     }
 }
