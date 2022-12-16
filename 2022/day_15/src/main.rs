ff2d259df96a69346847666ab13c4e977f072c46
Rubén <rubenjr0@proton.me>
2022-12-16 05:02:50 +0100

    ♻️ Day 15 final refactor

--- 2022/day_15/src/main.rs
+++ 2022/day_15/src/main.rs
@@ -1,114 +1,13 @@
 use std::{collections::HashSet, ops::RangeInclusive};
 
+use coord::Coord;
 use rayon::prelude::*;
+use sensor::Sensor;
 
+mod bounding_box_iter;
+mod coord;
+mod sensor;
 mod t;
-
-type Coord = (isize, isize);
-
-enum BoundingBoxIterationStatus {
-    Started,
-    BottomRight,
-    BottomLeft,
-    TopLeft,
-    TopRight,
-    Done,
-}
-
-struct BoundingBoxIterator {
-    cx: isize,
-    cy: isize,
-    x: isize,
-    y: isize,
-    status: BoundingBoxIterationStatus,
-}
-
-impl Iterator for BoundingBoxIterator {
-    type Item = Coord;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.status {
-            BoundingBoxIterationStatus::Started => {
-                self.status = BoundingBoxIterationStatus::BottomRight
-            }
-            BoundingBoxIterationStatus::BottomRight => {
-                self.x += 1;
-                self.y -= 1;
-                if self.y == self.cy {
-                    self.status = BoundingBoxIterationStatus::BottomLeft
-                }
-            }
-            BoundingBoxIterationStatus::BottomLeft => {
-                self.x -= 1;
-                self.y -= 1;
-                if self.x == self.cx {
-                    self.status = BoundingBoxIterationStatus::TopLeft
-                }
-            }
-            BoundingBoxIterationStatus::TopLeft => {
-                self.x -= 1;
-                self.y += 1;
-                if self.y == self.cy {
-                    self.status = BoundingBoxIterationStatus::TopRight
-                }
-            }
-            BoundingBoxIterationStatus::TopRight => {
-                self.x += 1;
-                self.y += 1;
-                if self.x == self.cx {
-                    self.status = BoundingBoxIterationStatus::Done
-                }
-            }
-            BoundingBoxIterationStatus::Done => return None,
-        }
-        Some((self.x, self.y))
-    }
-}
-
-#[derive(Debug)]
-struct Sensor {
-    x: isize,
-    y: isize,
-    distance: usize,
-}
-
-impl Sensor {
-    fn new(sc: Coord, bc: Coord) -> Self {
-        let distance = manhattan_distance(sc, bc);
-        Self {
-            x: sc.0,
-            y: sc.1,
-            distance,
-        }
-    }
-
-    fn contains(&self, p: Coord) -> bool {
-        manhattan_distance((self.x, self.y), p) <= self.distance
-    }
-
-    fn slice_at_row(&self, row: isize) -> Option<RangeInclusive<isize>> {
-        if self.y + (self.distance as isize) < row || row < self.y - self.distance as isize {
-            None
-        } else {
-            let h = self.y.abs_diff(row).abs_diff(self.distance);
-            Some(self.x - h as isize..=self.x + h as isize)
-        }
-    }
-
-    fn iter(&self) -> BoundingBoxIterator {
-        BoundingBoxIterator {
-            cx: self.x,
-            cy: self.y,
-            x: self.x,
-            y: self.y + self.distance as isize + 1,
-            status: BoundingBoxIterationStatus::Started,
-        }
-    }
-}
-
-fn manhattan_distance((x1, y1): Coord, (x2, y2): Coord) -> usize {
-    x1.abs_diff(x2) + y1.abs_diff(y2)
-}
 
 fn parse_entry(input: &str) -> (Sensor, Coord) {
     let input = input.replace(':', ",");
@@ -120,15 +19,9 @@ fn parse_entry(input: &str) -> (Sensor, Coord) {
         .split("y=")
         .skip(1)
         .map(|p| p.split(',').next().unwrap());
-    let sc = (
-        xs.next().unwrap().parse().unwrap(),
-        ys.next().unwrap().parse().unwrap(),
-    );
-    let bc = (
-        xs.next().unwrap().parse().unwrap(),
-        ys.next().unwrap().parse().unwrap(),
-    );
-    let sensor = Sensor::new(sc, bc);
+    let sc = Coord::parse(&mut xs, &mut ys);
+    let bc = Coord::parse(&mut xs, &mut ys);
+    let sensor = Sensor::new(sc, &bc);
     (sensor, bc)
 }
 
@@ -173,13 +66,13 @@ fn compute_row_coverage(sensors: &Vec<Sensor>, beacons: &Vec<Coord>, row: isize)
     let slices = merge_slices(slices);
     let obstructed: HashSet<_> = sensors
         .par_iter()
-        .filter(|s| s.y == row)
-        .map(|s| s.x)
+        .filter(|s| s.position.y == row)
+        .map(|s| s.position.x)
         .chain(
             beacons
                 .par_iter()
-                .filter(|(_, y)| y == &row)
-                .map(|(x, _)| *x),
+                .filter(|Coord { x: _, y }| y == &row)
+                .map(|Coord { x, y: _ }| *x),
         )
         .collect();
     slices
@@ -195,11 +88,12 @@ fn compute_row_coverage(sensors: &Vec<Sensor>, beacons: &Vec<Coord>, row: isize)
 
 fn part_two(sensors: &Vec<Sensor>, space: isize) -> Option<Coord> {
     for sensor in sensors {
-        for p in sensor.iter() {
-            if p.0 < 0 || p.1 < 0 || p.0 > space || p.1 > space {
+        let mut iter = sensor.iter();
+        while let Some(p) = iter.next() {
+            if p.x < 0 || p.y < 0 || p.x > space || p.y > space {
                 continue;
             }
-            if !sensors.iter().any(|s| s.contains(p)) {
+            if !sensors.iter().any(|s| s.contains(&p)) {
                 return Some(p);
             }
         }
@@ -217,7 +111,7 @@ fn main() {
     println!("Part one");
     println!(" - Solution {solution} found in {t_solution:?}\n");
 
-    let (Some((x, y)), t_solution) = timeit!(part_two(&sensors, 4_000_000)) else {
+    let (Some(Coord{x,y}), t_solution) = timeit!(part_two(&sensors, 4_000_000)) else {
         panic!("Couldn't find a solution!");
     };
     let solution = 4_000_000 * x + y;
@@ -229,11 +123,11 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use crate::{compute_row_coverage, merge_slices, parse_data, part_two, Sensor};
+    use crate::{compute_row_coverage, coord::Coord, merge_slices, parse_data, part_two, Sensor};
 
     #[test]
     fn test_slice() {
-        let sensor = dbg!(Sensor::new((0, 0), (1, 3)));
+        let sensor = dbg!(Sensor::new(Coord { x: 0, y: 0 }, &Coord { x: 1, y: 3 }));
         assert_eq!(sensor.slice_at_row(2), Some(-2..=2));
         assert_eq!(sensor.slice_at_row(-3), Some(-1..=1));
         assert_eq!(sensor.slice_at_row(4), Some(0..=0));
@@ -242,10 +136,10 @@ mod tests {
 
     #[test]
     fn test_borders() {
-        let sensor = dbg!(Sensor::new((0, 0), (0, 2)));
+        let sensor = dbg!(Sensor::new(Coord { x: 0, y: 0 }, &Coord { x: 0, y: 2 }));
         let points: Vec<_> = sensor.iter().collect();
         assert!(points.len() == 13);
-        assert!(!points.into_iter().any(|p| sensor.contains(p)))
+        assert!(!points.into_iter().any(|p| sensor.contains(&p)))
     }
 
     #[test]
@@ -267,6 +161,7 @@ mod tests {
         let input = include_str!("../test.txt");
         let (sensors, _) = parse_data(input);
         let solution = part_two(&sensors, 20);
-        assert_eq!(solution, Some((14, 11)));
+        let expected = Coord { x: 14, y: 11 };
+        assert_eq!(solution, Some(expected));
     }
 }
